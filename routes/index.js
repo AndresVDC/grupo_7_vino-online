30f7633a5d03f2d42e67f22e9896760f3fc8e0e9
EmilianoRosico <emiliano19@gmail.com>
2020-12-10 21:02:56 -0300

    Se crea proyecto con express-generator, se crean los partial e integran en los html

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get("/productCart", (req,res) => {
+  res.render('productCart',)
+})
+
+router.get("/productDetail", (req,res) => {
+  res.render('productDetail',)
+})
+
+router.get("/register", (req,res) => {
+  res.render('register',)
+})
+
+router.get("/login", (req,res) => {
+  res.render('login',)
+})
+
+module.exports = router;
