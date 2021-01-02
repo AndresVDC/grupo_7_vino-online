8efcaaaada4958d2364246ebc5d15d79d16cc68b
ezequiel1985 <ezequieljaureguiberry@hotmail.com>
2021-01-02 13:03:59 -0300

    En users.js se adaptan la rutas para trabajar con controlador, se crean una por get y otra post

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
+const usersController = require('../controllers/usersController');
 var router = express.Router();
 
-router.get("/register", (req,res) => {
-  res.render('users/register',)
-})
+router.get("/register", usersController.vista);
+router.post("/register", usersController.create);
 
 router.get("/login", (req,res) => {
   res.render('users/login',)
