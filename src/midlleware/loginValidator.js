bfcbefb908e799afd2f736f43782381d0fd409d1
ezequiel1985 <ezequieljaureguiberry@hotmail.com>
2021-02-07 20:00:49 -0300

    se crea midlleware para los errores de login y register

---
+++ src/midlleware/loginValidator.js
@@ -0,0 +1,6 @@
+var {check} = require ('express-validator')
+
+module.exports= [
+    check('email').isEmail().withMessage('El formato de email es invalido'),
+    check('password').isLength({min:4}).withMessage('La password debe tener 6 caracteres como minimo')
+  ]
