45939c5675913e61074afb9e76bd071867909410
ezequiel1985 <ezequieljaureguiberry@hotmail.com>
2021-03-12 22:24:44 -0300

    se modifica en el check el minimo requerido en la validacion

--- src/midlleware/registerValidator.js
+++ src/midlleware/registerValidator.js
@@ -1,8 +1,8 @@
 var {check} = require ('express-validator')
 
 module.exports= [
-    check('first_name').isLength({min:1}).withMessage('El campo nombre no puede estar vacio'),
-    check('last_name').isLength({min:1}).withMessage('El campo apellido no puede estar vacio'),  
+    check('first_name').isLength({min:3}).withMessage('El campo nombre no puede estar vacio'),
+    check('last_name').isLength({min:3}).withMessage('El campo apellido no puede estar vacio'),  
     check('email').isEmail().withMessage('El formato de email es invalido'),
     check('password').isLength({min:4}).withMessage('La password debe tener 4 caracteres como minimo')
   ]
