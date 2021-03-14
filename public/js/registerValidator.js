892f013e7c9bc8941ad45b5eb3ad8ed9923a1403
ezequiel1985 <ezequieljaureguiberry@hotmail.com>
2021-03-14 11:53:23 -0300

    EN register validator se cambia la variable errors de un [] a false

--- public/js/registerValidator.js
+++ public/js/registerValidator.js
@@ -1,31 +1,36 @@
 window.addEventListener('load', () => {
     let formRegister = document.querySelector('form.register');
     formRegister.addEventListener('submit', e => {
-        let errors = []
+        
+        let errors = false
         //valida campo nombre
         let campoName = document.querySelector('input.name');
         if(campoName.value == ""){
             document.querySelector('p.registerName').innerHTML = "No dejes el campo nombre vacio.";
             document.querySelector('p.registerName').style.color = 'red'
+            errors = true
         }
         //valida campo apellido
         let campoLastName = document.querySelector('input.surname');
         if(campoLastName.value == ""){
             document.querySelector('p.registerSurname').innerHTML = "No dejes el campo apellido vacio.";
             document.querySelector('p.registerSurname').style.color = 'red'
+            errors = true
         }
         // valida campo email
         let campoEmail = document.querySelector('input.email');
         if(campoEmail.value == ""){
             document.querySelector('p.registerEmail').innerHTML = "No dejes el campo email vacio.";
             document.querySelector('p.registerEmail').style.color = 'red'
+            errors = true
         }
         // valida campo password
         let campoPassword = document.querySelector('input.password');
         if(campoPassword.value == ""){
             document.querySelector('p.registerPassword').innerHTML = "No dejes el campo password vacio.";
             document.querySelector('p.registerPassword').style.color = 'red'
+            errors = true
         }
-        e.preventDefault();
+        errors == true ? e.preventDefault() : ''
     })
 })
