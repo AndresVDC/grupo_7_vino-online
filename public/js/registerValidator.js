af4caab394af352ea6454c7d7afbfc4894109c80
ezequiel1985 <ezequieljaureguiberry@hotmail.com>
2021-03-16 17:21:39 -0300

    se agrega validacion blur en register

--- public/js/registerValidator.js
+++ public/js/registerValidator.js
@@ -1,6 +1,60 @@
 window.addEventListener('load', () => {
+    // valida el campo nombre
+    let campoName = document.querySelector("input.name");
+    
+    campoName.addEventListener('blur', e =>{
+        if(campoName.value.length >= 2){
+          document.querySelector('p.registerName').innerHTML = "";
+        }else {
+          
+          document.querySelector('p.registerName').innerHTML = "El campo nombre requiere más de 2 caracteres";
+          document.querySelector('p.registerName').style.color = 'red'
+        }
+        e.preventDefault()
+      })
+    // valida campo apellido
+    let campoLastname = document.querySelector("input.surname");
+    
+    campoLastname.addEventListener('blur', e =>{
+        if(campoLastname.value.length >= 2){
+          document.querySelector('p.registerSurname').innerHTML = "";
+        }else {
+          
+          document.querySelector('p.registerSurname').innerHTML = "El campo apellido requiere más de 2 caracteres";
+          document.querySelector('p.registerSurname').style.color = 'red'
+        }
+        e.preventDefault()
+      })
+    // valida campo email
+    regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g // requiere que sea formato email
+    let email = document.querySelector("input.email");
+
+    email.addEventListener('blur', e =>{
+        if(email.value.match(regexEmail)){
+            document.querySelector('p.registerEmail').innerHTML = "";
+        }else {
+    
+            document.querySelector('p.registerEmail').innerHTML = "Completa el email con un formato valido";
+            document.querySelector('p.registerEmail').style.color = 'red'
+        }
+    e.preventDefault()
+    })
+    // valida campo password
+    let campoPassword = document.querySelector("input.password");
+    
+    campoPassword.addEventListener('blur', e =>{
+        if(campoPassword.value.length >= 6){
+          document.querySelector('p.registerPassword').innerHTML = "";
+        }else {
+          
+          document.querySelector('p.registerPassword').innerHTML = "La password requiere más de 6 caracteres";
+          document.querySelector('p.registerPassword').style.color = 'red'
+        }
+        e.preventDefault()
+      })
+    
     let formRegister = document.querySelector('form.register');
-    formRegister.addEventListener('submit', e => {
+    formRegister.addEventListener('submit', event => {
         
         let errors = false
         //valida campo nombre
@@ -31,6 +85,8 @@ window.addEventListener('load', () => {
             document.querySelector('p.registerPassword').style.color = 'red'
             errors = true
         }
-        errors == true ? e.preventDefault() : ''
+        errors == true ? event.preventDefault() : ''
     })
+        
+
 })
