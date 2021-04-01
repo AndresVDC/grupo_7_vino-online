ef0b09d768935a9d97ab524f72aca4cbf5f44bed
EmilianoRosico <emiliano19@gmail.com>
2021-04-01 19:03:02 -0300

    Se agrega al apiController.js el paginado para products

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,53 +1,81 @@
 const db = require('../database/models');
 const apiController = {
     //Responde la lista de productos mediante API con un formato JSON
-    products: (req, res) => {
-        db.Products.findAll()
-            .then(function (products) {
-                let category = [];
-                for (let index = 0; index < products.length; index++) {
-                    category.push(products[index].category)
-                    products[index].setDataValue("endpoint", "/api/products/" + products[index].id);
+    products: async (req, res) => {
+        //Verifica si se recibe un número de página.
+        if (req.params.pag == undefined) {
+            page = 1
+        }
+        else {
+            page = Number(req.params.pag)
+        }
+        //se almacena el total de productos por category
+        const countByCategory =
+            await Promise.all([
+                db.Products.count({ where: { category: 'Tinto' } }),
+                db.Products.count({ where: { category: 'Blanco' } }),
+                db.Products.count({ where: { category: 'Espumante' } }),
+                db.Products.count({ where: { category: 'Espirituoso' } }),
+            ]).then(values => {
+                let countByCategory = {
+                    tinto: values[0],
+                    blanco: values[1],
+                    espumante: values[2],
+                    espirituoso: values[3]
                 }
-                let answer = {
-                    meta: {
-                        status: "OK",
-                        count: products.length,
-                        countByCategory: { 
-                            tinto: category.filter(x => x=='Tinto').length,
-                            blanco: category.filter(x => x=='Blanco').length,
-                            espumante: category.filter(x => x=='Espumante').length,
-                            espirituoso: category.filter(x => x=='Espirituoso').length
-                        }
-                    },
-                    data: products,
-                }
-                res.json({ answer })
-            })
-            .catch((error) => {
+                return countByCategory;
+            }).catch(error => {
                 console.log(error)
-                res.json({meta: {status: "204 - Producto no encontrado"}})
             })
+        //Se guarda el total de productos.
+        const count = await db.Products.count().then(value => {
+            return value;
+        }).catch(error => {
+            console.log(error)
+        })
+        //se hace la consulta a la db con un limite de 10 productos y un offset basado en el N° de página.
+        const data = await db.Products.findAll({limit: 10, offset: (10*(page-1))}).then(products => {
+            for (let index = 0; index < products.length; index++) {
+                products[index].setDataValue("endpoint", "/api/products/" + products[index].id);
+            }
+            return products;
+        }).catch(error => { console.log(error) })
+
+        //Se confeccionan los links de nexy y previous.
+        let next = data.length == 10 ? "http://localhost:3001/api/products/page=" + (page + 1) : null;
+        let previous = page == 1 ? null : "http://localhost:3001/api/products/page=" + (page - 1);
+
+        //Se construye la respuesta.
+        let answer = {
+            meta: {
+                count: count,
+                countByCategory: countByCategory,
+                next: next,
+                previous: previous
+            },
+            data: data
+        }
+        res.json({ answer })
     },
     //Responde el detalle del producto mediante API con un formato JSON
-    productDetails: (req,res) => {
+    productDetails: (req, res) => {
         db.Products.findByPk(req.params.id)
             .then((product) => {
                 if (product != null) {
-                    res.json({ product: product})
+                    res.json({ product: product })
                 }
                 else {
-                   res.json({meta: {status: "204 - Producto no encontrado"}})
+                    res.json({ meta: { status: "204 - Producto no encontrado" } })
                 }
             }
             )
     },
     //Responde la lista de usuarios mediante API con un formato JSON
-    users:(req,res) =>{
+    users: (req, res) => {
 
     },
     //Responde el detalle del usuario mediante API con un formato JSON
-    userDetails: (req,res) => {
+    userDetails: (req, res) => {
 
     }
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const apiController = require('../controllers/apiController');
 
+router.get('/products/page=:pag?', apiController.products);
 router.get('/products', apiController.products);
 router.get('/products/:id', apiController.productDetails);
 router.get('/users', apiController.users);
